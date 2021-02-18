343c118317a34c09ca9d5176ed9c9875a67668d9
Julian Andrade <julian19896@gmail.com>
2021-02-18 12:57:57 -0500

    cambio url de los servicios para enlazarlo con heroku

--- src/app/config/config.ts
+++ src/app/config/config.ts
@@ -1,2 +1,2 @@
 // Exportamos la url que vamos a utlizar para accder al servidor
-export const URL_SERVICES = 'https://whispering-river-65925.herokuapp.com/';
+export const URL_SERVICES = 'https://whispering-river-65925.herokuapp.com';
