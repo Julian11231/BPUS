6eff405a7d7d99d5d4645cc85fd77a1f2baf7680
Julian Andrade <julian19896@gmail.com>
2021-02-18 10:52:26 -0500

    cambios para heroku

--- server.js
+++ server.js
@@ -1,16 +1,10 @@
-//Install express server
 const express = require('express');
-const path = require('path');
-
 const app = express();
 
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/bpus'));
+app.use(express.static('./dist/bpus'));
 
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/bpus/index.html'));
-});
+app.get('/*', (req, res) =>
+    res.sendFile('index.html', {root: 'dist/bpus/'}),
+);
 
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 4200);
+app.listen(process.env.PORT || 8080);
