7f9507dae865fd5f673cb252422c9dd6947ca79d
Julian Andrade <julian19896@gmail.com>
2021-02-19 16:02:50 -0500

    ajustes para el rol admin

--- src/app/login/requisitos/requisitos.component.ts
+++ src/app/login/requisitos/requisitos.component.ts
@@ -19,6 +19,8 @@ export class RequisitosComponent implements OnInit {
   icon: any = document.getElementsByClassName('fa');
   btnContinuar: any = document.getElementsByClassName('btn');
   info: any;
+  estudiante = JSON.parse(localStorage.getItem("estudiante"));
+  admin = JSON.parse(localStorage.getItem("administrativo"));
 
   // Inyectamos todos los servicios que necesitamos
   constructor(
@@ -37,17 +39,16 @@ export class RequisitosComponent implements OnInit {
 
     // Cargamos el porcentaje de los créditos cumplidos por el estudiante.
     this._modalidadesService.calcularPorcentaje();
+    if(this.estudiante){
+      this.info = this.estudiante;
+    }else if(this.admin && this.admin.rol == "ADMIN"){
+      this.info = this.admin;
+    }
 
-    this.getInfoEstudiante();
   }
 
   // Función que se llama cuando se da click en el botón "continuar"
   continuar() {
     this._loginService.dejaPasar();
-  }
-
-  // Asignamos a la variable global "info" la información del estudiante
-  getInfoEstudiante() {
-    this.info = JSON.parse(localStorage.getItem('estudiante'));
   }
 }
